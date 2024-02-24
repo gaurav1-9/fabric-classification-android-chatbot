b70dd95c66eec4aaad88e2756cefb2aa01634466
Gaurav <gauravkrdas19@gmail.com>
2024-02-24 15:21:23 +0530

    slection handlers

--- project_application/lib/main.dart
+++ project_application/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import './global_properties/app_colors.dart';
 import './screens/home_screen.dart';
 
 void main() {
@@ -18,6 +19,9 @@ class AndroidChatbot extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme: GoogleFonts.quicksandTextTheme(),
+        textSelectionTheme: const TextSelectionThemeData(
+            selectionColor: AppColor.raisinBlack,
+            selectionHandleColor: AppColor.raisinBlack),
       ),
       home: const HomeScreen(),
     );
